80aafd73d7df6115535bad873778a66838d0f4d0
Wesley Merkel <ooesili@gmail.com>
2013-12-31 01:42:18 -0700

    Huge xmonad overhaul to integrate solarized theme
    This is a big change to the xmonad/xmobar configuration that, among
    other things, integrates more solarized colors into visual elements of
    the program. Other changes include using dmenu as the program launcher,
    adding more monitors to xmobar, and adding an icon to xmobar that will
    switch to a random wallpaper when clicked.

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -2,7 +2,7 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig
 import System.IO
 import XMonad.Prompt
@@ -26,13 +26,11 @@ main = do
         { terminal          = myTerminal
         , manageHook        = myManageHook
         , layoutHook        = myLayoutHook
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "#859900" "" . shorten 75
-                        }
+        , logHook           = myLogHook xmproc
         , workspaces        = myWorkspaces
         , normalBorderColor = myNormalBorderColor
         , focusedBorderColor = myFocusedBorderColor
+        , borderWidth = 1
         } `additionalKeysP`
         [ ("M-d",   spawn "firefox")
         , ("M-g",   spawn "gvim")
@@ -50,7 +48,7 @@ main = do
         , ("M-e",   spawn "easytag")
         , ("M-s",   spawn "scide")
         , ("M-f",   sendMessage ToggleLayout)
-        , ("M-p",   shellPrompt  xoriaXPConfig)
+        , ("M-p",   spawn "dmenu_run -b -fn '-xos4-terminus-medium-r-normal--14-140-72-72-c-80-iso8859-1' -p 'run>' -nb '#002b36' -nf '#93a1a1' -sb '#dc322f' -sf '#fdf6e3'")
         , ("M-S-a", withFocused $ keysMoveWindowTo (1918,18) (1, 0))
         ] `additionalKeys`
         [ ((0, 0x1008ff13), spawn "amixer --quiet set Master 1+")
@@ -58,18 +56,58 @@ main = do
         , ((0, 0x1008ff12), spawn "amixer --quiet set Master toggle")
         ]
 
+-- solarized colors
+data SolarizedPalette = SolarizedPalette
+    { base03  :: String
+    , base02  :: String
+    , base01  :: String
+    , base00  :: String
+    , base0   :: String
+    , base1   :: String
+    , base2   :: String
+    , base3   :: String
+    , yellow  :: String
+    , orange  :: String
+    , red     :: String
+    , magenta :: String
+    , violet  :: String
+    , blue    :: String
+    , cyan    :: String
+    , green   :: String
+    }
+
+solarP = SolarizedPalette
+    { base03 =  "#002b36"
+    , base02 =  "#073642"
+    , base01 =  "#586e75"
+    , base00 =  "#657b83"
+    , base0 =   "#839496"
+    , base1 =   "#93a1a1"
+    , base2 =   "#eee8d5"
+    , base3 =   "#fdf6e3"
+    , yellow =  "#b58900"
+    , orange =  "#cb4b16"
+    , red =     "#dc322f"
+    , magenta = "#d33682"
+    , violet =  "#6c71c4"
+    , blue =    "#268bd2"
+    , cyan =    "#2aa198"
+    , green =   "#859900"
+    }
+
+
 -- simple items
 
 myTerminal = "urxvtc"
 
 myWorkspaces = ["home","web","apps","vim","term","android","misc","mixer","ardour"]
 
-myNormalBorderColor = "#505050"
+myNormalBorderColor = base02 solarP
 
-myFocusedBorderColor = "#0095b9"
+myFocusedBorderColor = red solarP
 
 -- layout configuration
-myTabbed = noBorders $ tabbed shrinkText myTabTheme
+myTabbed = noBorders $ tabbed shrinkText solarizedTabs
 
 androidLayout =
     let mainPanes  = TwoPane 0 (719/960)
@@ -82,7 +120,7 @@ mixerLayout r =
         sidePane = Mirror $ TwoPane 0 (9/10)
         name = renamed [Replace "Mixer Layout"]
         notPatchOrCtl = Not $ Or (Resource "qjackctl") (Title "Audio Connection Manager")
-        mixerTabs = tabbed shrinkText myTabTheme
+        mixerTabs = tabbed shrinkText solarizedTabs
     in name $ combineTwoP mainPanes mixerTabs sidePane notPatchOrCtl
 
 myLayoutHook =
@@ -99,15 +137,15 @@ myLayoutHook =
      $ (tiled ||| Mirror tiled ||| myTabbed)
 
 -- tabbed layout theme
-myTabTheme = defaultTheme
-    { activeColor         = "#3a3a3a"
-    , inactiveColor       = "#1c1c1c"
-    , activeBorderColor   = "#0095b9"
-    , inactiveBorderColor = "#707070"
-    , activeTextColor     = "#ffffff"
-    , inactiveTextColor   = "#9e9e9e"
+solarizedTabs = defaultTheme
+    { activeColor         = base01 solarP
+    , inactiveColor       = base02 solarP
+    , activeBorderColor   = myFocusedBorderColor
+    , inactiveBorderColor = myNormalBorderColor
+    , activeTextColor     = base2 solarP
+    , inactiveTextColor   = base0 solarP
     , fontName            = "-xos4-terminus-medium-r-normal--14-140-72-72-c-80-iso8859-1"
-    , decoHeight          = 18
+    , decoHeight          = 16
     }
 
 -- manageHook configuration
@@ -127,13 +165,10 @@ myManageHook = composeOne
     , return True			-?> manageDocks
     ]
 
--- set the look for the prompts
-xoriaXPConfig = defaultXPConfig
-    { font = "-xos4-terminus-medium-r-normal--14-140-72-72-c-80-iso8859-1"
-    , bgColor         = "#1c1c1c"
-    , fgColor         = "#d0d0d0"
-    , fgHLight        = "#eeeeee"
-    , bgHLight        = "#657b83"
-    , borderColor     = "#4e4e4e"
-    , alwaysHighlight = True
+-- logHook configuration
+myLogHook input = dynamicLogWithPP xmobarPP
+    { ppOutput = hPutStrLn input
+    , ppTitle = xmobarColor (green solarP) "" . shorten 82
+    , ppLayout = xmobarColor (yellow solarP) ""
+    , ppCurrent = xmobarColor (base2 solarP) (base01 solarP) . wrap "[" "]"
     }
