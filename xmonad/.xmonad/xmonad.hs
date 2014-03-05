4fe5c49124b9cbc6109fc341183e0ef274ccafcc
Wesley Merkel <ooesili@gmail.com>
2014-03-04 20:30:26 -0700

    Update xmonad and xmobar configurations
    Replaced weather in xmobar with MPD information. Added spacing around
    windows in xmonad. Add grid menu for launching applications,
    manipulating windows and quiting xmonad. Add other minor layout changes.

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageHelpers
@@ -14,39 +15,39 @@ import XMonad.Layout.ComboP
 import XMonad.Layout.TwoPane
 import XMonad.Layout.Gaps
 import XMonad.Layout.Renamed
+import XMonad.Layout.Spacing
 import XMonad.Actions.FloatKeys
+import XMonad.Actions.GridSelect
+import XMonad.Actions.WindowBringer
+import System.Exit
 import System.Environment (getEnv)
 
 main = do
     home <- io $ getEnv "HOME"
     xmproc <- spawnPipe $ "xmobar " ++ home ++ "/.xmonad/xmobarrc"
     xmonad $ defaultConfig
-        { terminal          = myTerminal
-        , manageHook        = myManageHook
-        , layoutHook        = myLayoutHook
-        , logHook           = myLogHook xmproc
-        , workspaces        = myWorkspaces
-        , normalBorderColor = myNormalBorderColor
+        { terminal           = myTerminal
+        , manageHook         = myManageHook
+        , layoutHook         = myLayoutHook
+        , logHook            = myLogHook xmproc
+        , workspaces         = myWorkspaces
+        , normalBorderColor  = myNormalBorderColor
         , focusedBorderColor = myFocusedBorderColor
-        , borderWidth = 1
+        , borderWidth        = 2
         } `additionalKeysP`
         [ ("M-d",   spawn "firefox")
-        , ("M-g",   spawn "gvim")
-        , ("M-S-o", spawn "systemctl poweroff")
-        , ("M-S-r", spawn "systemctl reboot")
-        , ("M-S-s", spawn "systemctl suspend")
+        , ("M-S-q", shutdownGrid)
+        , ("M-o",   appGrid $ home ++ "/.xmonad/apps")
+        , ("M-g",   gotoMenuArgs  (dmenuArgs "goto>" yellow  ++ ["-l", "30"]))
+        , ("M-S-g", bringMenuArgs (dmenuArgs "grab>" magenta ++ ["-l", "30"]))
         , ("M-z",   spawn "ncmpcpp prev")
         , ("M-x",   spawn "ncmpcpp play")
         , ("M-c",   spawn "ncmpcpp pause")
         , ("M-v",   spawn "ncmpcpp stop")
         , ("M-b",   spawn "ncmpcpp next")
         , ("M-i",   spawn "inithome")
-        , ("M-r",   spawn "qjackctl")
-        , ("M-a",   spawn "anki")
-        , ("M-e",   spawn "easytag")
-        , ("M-s",   spawn "scide")
         , ("M-f",   sendMessage ToggleLayout)
-        , ("M-p",   spawn "dmenu_run -fn '-xos4-terminus-medium-r-normal--12-120-72-72-c-60-iso8859-1' -p 'run>' -nb '#002b36' -nf '#93a1a1' -sb '#dc322f' -sf '#fdf6e3'")
+        , ("M-p",   spawn $ "dmenu_run " ++ unwords (map (wrap "'" "'") $ dmenuArgs "run>" green))
         , ("M-S-a", withFocused $ keysMoveWindowTo (1918,18) (1, 0))
         ---------- TMUX REMAPPINGS ----------
         -- swich panes
@@ -124,7 +125,7 @@ myNormalBorderColor = base02 solarP
 myFocusedBorderColor = red solarP
 
 -- layout configuration
-myTabbed = noBorders $ tabbed shrinkText solarizedTabs
+myTabbed = noBorders . renamed [Replace "Tabbed"] $ tabbed shrinkText solarizedTabs
 
 androidLayout =
     let mainPanes  = TwoPane 0 (719/960)
@@ -144,19 +145,20 @@ myLayoutHook =
     let showToolbar = gaps [(U,16)]
         tiled = Tall 1 (3/100) (1/2)
         ardourLayout = renamed [Replace "Ardour Tabs"] (showToolbar myTabbed ||| myTabbed)
+        spaced = renamed [CutWordsLeft 2] . smartSpacing 8
     in smartBorders
      . onWorkspace "ardour" ardourLayout
-     . toggleLayouts (Full ||| myTabbed)
+     . toggleLayouts (Full ||| renamed [PrependWords "Full"] myTabbed)
      . showToolbar
      . onWorkspace "android" androidLayout
      . onWorkspace "mixer" mixerLayout
-     $ (tiled ||| Mirror tiled ||| myTabbed)
+     $ (spaced tiled ||| spaced (Mirror tiled) ||| myTabbed)
 
 -- tabbed layout theme
 solarizedTabs = defaultTheme
     { activeColor         = base01 solarP
     , inactiveColor       = base02 solarP
-    , activeBorderColor   = myFocusedBorderColor
+    , activeBorderColor   = base01 solarP
     , inactiveBorderColor = myNormalBorderColor
     , activeTextColor     = base2 solarP
     , inactiveTextColor   = base0 solarP
@@ -167,7 +169,8 @@ solarizedTabs = defaultTheme
 -- manageHook configuration
 myManageHook = composeOne
     [ isFullscreen			-?> doFullFloat
-    , className =? "MPlayer"		-?> doFloat
+    , className =? "MPlayer" <||>
+        isSCWindow			-?> doFloat
     , resource =? "ardour_editor"	-?> doShift "ardour"
     , resource =? "ardour_mixer" <||>
         title =? "Audio Connection Manager" <||>
@@ -180,6 +183,10 @@ myManageHook = composeOne
         resource =? "ardour-3.4"	-?> doCenterFloat
     , return True			-?> manageDocks
     ]
+    where isSCWindow = do
+              titleStart <- fmap (take 18) title
+              let isNotHelp = return (titleStart /= "SuperCollider Help")
+              resource =? "SuperCollider" <&&> isNotHelp
 
 -- logHook configuration
 myLogHook input = dynamicLogWithPP xmobarPP
@@ -188,3 +195,34 @@ myLogHook input = dynamicLogWithPP xmobarPP
     , ppLayout = xmobarColor (yellow solarP) ""
     , ppCurrent = xmobarColor (base2 solarP) (base01 solarP) . wrap "[" "]"
     }
+
+-- custom GridSelect config for strings
+solarGSC colorF = (buildDefaultGSConfig colorizer) { gs_font = normalFont }
+    where colorizer _ isActive =
+              return $ if isActive
+                       then (colorF solarP, base3 solarP)
+                       else (base02 solarP, base1 solarP)
+
+-- shutdown grid
+shutdownGrid = runSelectedAction (solarGSC red) actions
+    where actions = [ ("logout",     io (exitWith ExitSuccess))
+                    , ("poweroff",   spawn "systemctl poweroff")
+                    , ("reboot",     spawn "systemctl reboot")
+                    , ("suspend",    spawn "systemctl suspend")
+                    , ("hybernate",  spawn "systemctl hybrid-sleep")
+                    ]
+
+-- commonly used application grid
+appGrid file = do
+    let go h = (hGetContents h) >>= (return . lines $!)
+    apps <- io $ withFile "/home/ooesili/.xmonad/apps" ReadMode go
+    spawnSelected (solarGSC green) apps
+
+-- dmenu argments
+dmenuArgs prompt colorF = [ "-fn", "-xos4-terminus-medium-r-normal--12-120-72-72-c-60-iso8859-1"
+                          , "-p",  prompt
+                          , "-nb", base03 solarP
+                          , "-nf", base1  solarP
+                          , "-sb", colorF solarP
+                          , "-sf", base3  solarP
+                          ]
