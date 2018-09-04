e3c6f1c2efc7e6de956e6f0a39af788a20b3099c
Wesley Merkel <ooesili@gmail.com>
2018-09-04 01:58:00 -0500

    Add udev rule for the Axe-FX II USB firmware

--- system/configuration.nix
+++ system/configuration.nix
@@ -41,6 +41,24 @@ in with lib; {
     unzip
   ];
 
+  services.udev.extraRules =
+    let axefx2-firmware = pkgs.stdenvNoCC.mkDerivation {
+      name = "axefx2-firmware";
+      src = pkgs.fetchurl {
+        url = https://launchpad.net/~albaguirre/+archive/ubuntu/axe-fx2/+sourcefiles/axefx2-usb-firmware/1.0/axefx2-usb-firmware_1.0.tar.xz;
+        sha256 = "0dhawalyh9ah8snymisa1jm1l5n9wjyifp9ws0bhsypl7sv2fhy9";
+      };
+
+      phases = [ "unpackPhase" "installPhase" ];
+      installPhase = ''
+        install -Dm0755 axefx2-usb-fw.hex "$out/share/usb/axefx2.hex"
+      '';
+    };
+    in ''
+      # Fractal Audio Systems Axe-FX II
+      ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="2466", ATTR{idProduct}=="0003", RUN+="${pkgs.fxload}/bin/fxload -t fx2 -I ${axefx2-firmware}/share/usb/axefx2.hex -D $env{DEVNAME}"
+    '';
+
   fonts.fonts = with pkgs; [
     hack-font
   ];
