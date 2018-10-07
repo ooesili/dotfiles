0fb7851431e9af3665b970f80c3c2efde910e9a2
Wesley Merkel <ooesili@gmail.com>
2018-10-07 15:10:02 -0500

    Upgrade to NixOS 18.09

--- system/base.nix
+++ system/base.nix
@@ -187,7 +187,7 @@ in with lib; {
     # compatible, in order to avoid breaking some software such as database
     # servers. You should change this only after NixOS release notes say you
     # should.
-    system.stateVersion = "18.03"; # Did you read the comment?
+    system.stateVersion = "18.09"; # Did you read the comment?
 
     time.timeZone = "America/Chicago";
 
