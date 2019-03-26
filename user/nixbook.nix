817b8766e3b9cc4df7b2332af94bd6bfe5d6d29c
Wesley Merkel <ooesili@gmail.com>
2019-03-25 23:03:24 -0500

    Bump up terminal font on nixbook

--- user/nixbook.nix
+++ user/nixbook.nix
@@ -1,7 +1,7 @@
 import ./base.nix {
   pkgs = import <nixpkgs> {};
   config = {
-    alacritty.font.size = "6.0";
+    alacritty.font.size = "7.0";
     xmodmap.enable = true;
     xmodmap.swapAltSuper = false;
   };
