b01159bcf40a6766a9e9772528b3ddda02e8ca39
Wesley Merkel <ooesili@gmail.com>
2019-08-26 00:57:45 -0500

    Fix incorrect placement of system.stateVersion

--- system/nixbox.nix
+++ system/nixbox.nix
@@ -14,4 +14,10 @@
 
   networking.hostName = "nixbox";
   services.mpd.musicDirectory = "/home/ooesili/exthd/files/music";
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.03"; # Did you read the comment?
 }
