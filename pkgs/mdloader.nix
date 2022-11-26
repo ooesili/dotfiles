97e095ce7d966b67f655657c2292c4d97007026e
Wesley Merkel <ooesili@gmail.com>
2022-11-25 17:52:11 -0700

    Update mdloader: 1.0.6 -> 1.0.7

--- pkgs/mdloader.nix
+++ pkgs/mdloader.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation {
   pname = "mdloader";
-  version = "1.0.6";
+  version = "1.0.7";
 
   src = fetchFromGitHub {
     owner = "Massdrop";
     repo = "mdloader";
-    rev = "e4f977416994f54ca3f8d2e72f2b225d52f7c42e";
-    sha256 = "sha256-/hRS6T+/cy1QQfFAsBYdZjooDDCBP9ztRn1GtapVQMY=";
+    rev = "aeee18977d113d2f5d3a1df0dcdcea53d4f230b7";
+    sha256 = "sha256-ydi9XMHztmechVEfXD5gT0Solk3D2CGAp69xvXK5iYk=";
   };
 
   installPhase = ''
@@ -19,6 +19,5 @@ stdenv.mkDerivation {
     description = "Massdrop Firmware Loader - for CTRL / ALT / SHIFT / Rocketeer keyboards";
     homepage = "https://github.com/Massdrop/mdloader";
     license = lib.licenses.gpl3;
-    maintainers = [ lib.maintainers.eyjhb ];
   };
 }
