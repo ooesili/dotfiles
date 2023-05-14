9dcf9eb51db29f1270b438f3b60b24127bd2bc28
Wesley Merkel <ooesili@gmail.com>
2023-05-14 16:01:11 -0600

    feat: enable virtualbox on nixbox

--- system/nixbox/configuration.nix
+++ system/nixbox/configuration.nix
@@ -77,6 +77,8 @@
     '';
   };
 
+  virtualisation.virtualbox.host.enable = true;
+
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
